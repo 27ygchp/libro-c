622e255f3f17764aec594489210f17b8a1243e71
Diego Karabin <diegokarabin@gmail.com>
2023-06-20 16:37:30 -0400

    correcciones 3.35-bing.c

--- 3er Capitulo/3.35-bing.c
+++ 3er Capitulo/3.35-bing.c
@@ -1,36 +1,35 @@
 /* Un palíndromo es un número o una frase de texto que se lee igual hacia delante y hacia atrás. Por ejemplo, cada
 uno de los siguientes números de cinco dígitos, son palíndromos: 12321, 55555, 45554, y 11611. Escriba un
- programa que lea números de cinco dígitos y que determine si es o no, un palíndromo. [Pista: Utilice los 
+ programa que lea números de cinco dígitos y que determine si es o no, un palíndromo. [Pista: Utilice los
  operadores de división y residuo para separar el número en sus dígitos individuales.]  */
 
 
 #include<stdio.h>
 
-int main(){
+int main() {
+    int numero, temporal;
+    int contador = 0;
 
-int numero, temporal;
-int contador = 0; 
+    printf("Ingrese un numero de 5 digitos: ");
+    scanf("%d", &numero);
 
-printf("Ingrese un numero de 5 digitos: ");
-scanf("%d", &numero); 
+    temporal = numero;
 
-temporal = numero; 
+    while (temporal != 0)
+    {
+        contador *= 10;
+        contador += temporal % 10;
+        temporal /= 10;
 
-while (temporal != 0)
-{
-    contador *= 10;
-    contador += temporal % 10; 
-    temporal /= 10; 
-     
-} 
+    }
 
-		if (numero == contador){
-		
-			printf("Es polindromo"); 
-	} else{
-	
-			printf("No es polindromo"); 
-}
+    if (numero == contador){
 
-    return 0; 
+        printf("Es polindromo");
+    } else{
+
+        printf("No es polindromo");
+    }
+
+    return 0;
 }
