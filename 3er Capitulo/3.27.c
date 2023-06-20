446fd7cc5b0ae3143074eaf33b7bf98694e605ec
Diego Karabin <diegokarabin@gmail.com>
2023-06-20 13:55:12 -0400

    Correcciones 3.27.c

--- 3er Capitulo/3.27.c
+++ 3er Capitulo/3.27.c
@@ -1,32 +1,32 @@
-/* Mediante un método similar al del ejercicio 3.24, encuentre los dos valores más grandes de los 10 números. 
+/* Mediante un método similar al del ejercicio 3.24, encuentre los dos valores más grandes de los 10 números.
 [Nota: Debe introducir un número a la vez.]*/
 
 #include<stdio.h>
 
 int main(){
 
-int mayor1 = 0, mayor2 = 0, numero = 0; 
-int contador = 0;  
+int mayor1 = 0, mayor2 = 0, numero = 0;
+int contador = 0;
 
 while (contador < 10)
 {
-    printf("Introduzca 10 numeros: "); 
-    scanf("%d", &numero); 
-    
+    printf("Introduzca 10 numeros: "); // Mejorar este mensaje Ej: Introduzca un numero (1/10)
+    scanf("%d", &numero);
+
 
     if( numero > mayor1){
         mayor2 = mayor1;
-        mayor1 = numero; 
-    } 
+        mayor1 = numero;
+    }
 
      else if (numero > mayor2 )
     {
-       	mayor2 = numero;   
+       	mayor2 = numero;
     }
-    
-    contador++; 
+
+    contador++;
 }
 
-    printf("Los dos numeros mayores son: %d y %d", mayor1, mayor2); 
-    return 0; 
+    printf("Los dos numeros mayores son: %d y %d", mayor1, mayor2);
+    return 0;
 }
