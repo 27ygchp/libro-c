ee860d272cb619b942e07bccfa399168690284b2
gladiola <gladiolaprieto938@gmail.com>
2023-06-21 18:30:17 -0430

    cambios

--- 2do Capitulo/2.30.c.c
+++ 2do Capitulo/2.30.c.c
@@ -7,20 +7,20 @@ int main(){
 	printf("Ingrese un numero de cinco digitos: ");
 	scanf("%d", &numero);
 	
-	numero = numero1 / 10000;
-	numero1 %= 10000; 
+	numero1 = numero / 10000;
+	numero %= 10000; 
 
-	numero = numero2 / 1000;
-	numero2 %= 1000;
+	numero2 = numero / 1000;
+	numero %= 1000;
 
-	numero = numero3 / 100;
-	numero3 %= 100;
+	numero3 = numero / 100;
+	numero %= 100;
 
-	numero = numero4 / 10;
-	numero4 %= 10;
+	numero4 = numero / 10;
+	numero %= 10;
 
-	numero = numero5 / 1;
-	numero5 %= 1;
+	numero5 = numero / 1;
+	numero %= 1;
 
 	printf("%d   %d   %d   %d   %d\n",
 	 numero1, numero2, numero3, numero4, numero5);
