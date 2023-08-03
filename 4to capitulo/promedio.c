6b9a5868f40abe5fa23f6f656d942b4a61c929e4
gladiola <gladiolaprieto938@gmail.com>
2023-08-02 21:46:29 -0430

    nuevo ejercicio no terminado

--- 4to capitulo/promedio.c
+++ 4to capitulo/promedio.c
@@ -2,8 +2,25 @@
 
 int main(){
 
+int calificacion = 0, total = 0, contador; 
+float promedio = 0; 
 
-
-
-    return 0; 
+for( contador = 1; contador ; contador++){
+	
+	while( calificacion != -1){
+		
+    printf("Ingrese la calificacion, -1 para finalizar: "); 
+	scanf("%d", &calificacion);
+		
+		total += calificacion; 
+	}
+	
+	if(calificacion != 0){
+		
+		promedio = (float)total / contador;  
+	 
+	}
+}
+	printf("El promedio del grupo es: %.2f\n", promedio);
+	return 0; 
 }
